f2d6059f96f729ed7de24eb5da2bcb2305334175
NaderWD <Nader.webdevop@gmail.com>
2025-01-25 13:19:37 +0330

    Moving Front Templates

---
+++ NaderResume.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NaderResume.Web.Areas.Admin.Controllers
+{
+    [Route("admin/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NaderResume.Web/Components/HeaderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NaderResume.Web.Components
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        #region Methods
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("Header");
+        }
+
+        #endregion
+    }
+}
