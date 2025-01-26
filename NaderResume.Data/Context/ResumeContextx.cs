30dcef64e4dfcca4138693ddc9ab9158e990ae78
NaderWD <Nader.webdevop@gmail.com>
2025-01-26 10:05:59 +0330

    User Model Creation

---
+++ NaderResume.Data/Context/ResumeContextx.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NaderResume.Data.Context
+{
+    public class ResumeContextx : DbContext
+    {
+
+    }
+}
---
+++ NaderResume.Data/Models/Users/User.cs
@@ -0,0 +1,16 @@
+﻿namespace NaderResume.Data.Models.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? Password2 { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
