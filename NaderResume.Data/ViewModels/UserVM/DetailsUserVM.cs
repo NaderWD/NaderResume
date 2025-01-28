812053a9c44a0f0dceb709556cef2e08bc084ddd
NaderWD <Nader.webdevop@gmail.com>
2025-01-28 08:58:06 +0330

    View Models Completed

--- NaderResume.Data/ViewModels/UserVM/DetailsUserVM.cs
+++ NaderResume.Data/ViewModels/UserVM/DetailsUserVM.cs
@@ -1,6 +1,30 @@
-﻿namespace NaderResume.Data.ViewModels.UserVM
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NaderResume.Data.ViewModels.UserVM
 {
     public class DetailsUserVM
     {
+        [DisplayName("نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        public string? FirstName { get; set; }
+
+        [DisplayName("نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        public string? LastName { get; set; }
+
+        [DisplayName("ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(150)]
+        public string? Email { get; set; }
+
+        [DisplayName("تلفن همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(15)]
+        public string? Phone { get; set; }
     }
 }
--- NaderResume.Data/ViewModels/UserVM/UpdateUserVM.cs
+++ NaderResume.Data/ViewModels/UserVM/UpdateUserVM.cs
@@ -1,6 +1,44 @@
-﻿namespace NaderResume.Data.ViewModels.UserVM
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NaderResume.Data.ViewModels.UserVM
 {
     public class UpdateUserVM
     {
+        [DisplayName("نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        public string? FirstName { get; set; }
+
+        [DisplayName("نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        public string? LastName { get; set; }
+
+        [DisplayName("ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(150)]
+        public string? Email { get; set; }
+
+        [DisplayName("تلفن همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(15)]
+        public string? Phone { get; set; }
+
+        [DisplayName("کلمه ی عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [MaxLength(100)]
+        public string? Password { get; set; }
+
+        [DisplayName("تکرار کلمه ی عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string? Password2 { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
